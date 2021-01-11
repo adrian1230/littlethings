0fac6f3ab36c79bc4b3d44839c5bb28f5a5fa7e6
adrian1230 <adrian1145d98@gmail.com>
2021-01-12 02:31:26 +0800

    adrian

--- rollout.py
+++ rollout.py
@@ -5,48 +5,55 @@ def rollOut(
     length:'length of array',
     first:'only s: singular or p: plural'):
     array = [start]
+    # if True:
     if first.lower() == 's':
         value = start
         c = 0
+        # 1 4 10
+        #  3 6
         while len(array) != length:
             last = array[-1]
             c += 1
-            value += 1
-            if c % 2 != 0:
-                if value % 2 == 0:
-                    pass
+            length = len(array)
+            # value += 1
+            while len(array) == length:
+                if c % 2 != 0:
+                    value += 1
+                    if value % 2 == 0:
+                        pass
+                    else:
+                        if value > last:
+                            array.append(last+value)
+                        else:
+                            pass
                 else:
-                    if value > last:
-                        array.append(last+value)
+                    value += 1
+                    if value % 2 == 0:
+                        if value > last:
+                            array.append(last+value)
+                        else:
+                            pass
                     else:
                         pass
-            else:
-                if value % 2 == 0:
-                    if value > last:
-                        array.append(last+value)
-                    else:
-                        pass
-                else:
-                    pass
-    elif first.lower() == "p":
-        val = start
-        d = 0
-        while len(array) != length:
-            final = array[-1]
-            d += 1
-            val += 1
-            if d % 2 != 0:
-                if val % 2 == 0:
-                    if value > final:
-                        array.append(final+val)
-                else:
-                    pass
-            else:
-                if val % 2 == 0:
-                    pass
-                else:
-                    if value > final:
-                        array.append(final+val)
+    # elif first.lower() == "p":
+    #     val = start
+    #     d = 0
+    #     while len(array) != length:
+    #         final = array[-1]
+    #         d += 1
+    #         val += 1
+    #         if d % 2 != 0:
+    #             if val % 2 == 0:
+    #                 if value > final:
+    #                     array.append(final+val)
+    #             else:
+    #                 pass
+    #         else:
+    #             if val % 2 == 0:
+    #                 pass
+    #             else:
+    #                 if value > final:
+    #                     array.append(final+val)
     print(array)
 
 rollOut(int(sys.argv[1]),int(sys.argv[2]),sys.argv[3])
