4f0014a41bbe017d6afb7042d004d0b5237ebe87
adrian1230 <adrian1145d98@gmail.com>
2021-01-13 20:31:06 +0800

    adrian

--- c++/prime.cpp
+++ c++/prime.cpp
@@ -12,7 +12,7 @@ int main()
         isPrime = false;
     }
     else {
-        for (i == 2; i <= n /2; ++i) {
+        for (i = 2; i <= n /2; ++i) {
             if (n % i == 0) {
                 isPrime = false;
                 break;
