194a83d402067d5566b1b612cbde83c7b298d311
adrian1230 <adrian1145d98@gmail.com>
2021-01-13 14:59:13 +0800

    adrian

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # littlethings
+
+non-competitive programming
+not-timed
---
+++ c++/l.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter an int: ";
+    cin >> number;
+
+    cout << number << "\n";
+    cout << "bye\n";
+    return 0;
+}
