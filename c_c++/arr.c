64fd81d1f6bcfee866a1e9ef3ddb1bdc23d91a6a
adrian1230 <adrian1145d98@gmail.com>
2021-01-14 22:11:58 +0800

    adrian

--- c_c++/arr.c
+++ c_c++/arr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <vector>
+#include <array>
 /*
 Given a 2D array arr[][] with each row of the form {l, r}, 
 the task is to find a pair (i, j) such that 
@@ -13,10 +14,9 @@ int main()
   printf("enter the length of the integer list: ");
   int n;
   scanf("%d",&n);
-  int data[n];
+  int data[n][2];
   for (int i = 0; i < n; ++i)
   {
-    printf("n's val must smaller than (n + 1)'s val where n is an order number");
     int arr[2];
     printf("the first value: ");
     int val_1;
@@ -26,7 +26,6 @@ int main()
     scanf("%d",&val_2);
     arr[0] = val_1;
     arr[1] = val_2;
-    // data[i] = arr;
   }
   return 0;
 }
