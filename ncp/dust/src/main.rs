2526806df7199f028cfa1dd3e323c854157670cf
adrian1230 <adrian1145d98@gmail.com>
2021-01-15 04:23:59 +0800

    adrian

--- ncp/dust/src/main.rs
+++ ncp/dust/src/main.rs
@@ -1,6 +1,8 @@
 mod jo;
+mod arr;
 
 fn main() {
    jo::joke();
    jo::haha();
+   arr::list();
 }
--- ncp/dust/target/debug/deps/dust.d
+++ ncp/dust/target/debug/deps/dust.d
@@ -1,6 +1,7 @@
-/Users/qwe/Desktop/oult/ncp/dust/target/debug/deps/dust: src/main.rs src/jo.rs
+/Users/qwe/Desktop/oult/ncp/dust/target/debug/deps/dust: src/main.rs src/jo.rs src/arr.rs
 
-/Users/qwe/Desktop/oult/ncp/dust/target/debug/deps/dust.d: src/main.rs src/jo.rs
+/Users/qwe/Desktop/oult/ncp/dust/target/debug/deps/dust.d: src/main.rs src/jo.rs src/arr.rs
 
 src/main.rs:
 src/jo.rs:
+src/arr.rs:
