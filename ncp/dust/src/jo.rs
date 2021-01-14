888f3ad62f6dc288b77cdc9ec270d8a1ed174432
adrian1230 <adrian1145d98@gmail.com>
2021-01-15 04:11:32 +0800

    adrian

--- ncp/dust/src/jo.rs
+++ ncp/dust/src/jo.rs
@@ -5,4 +5,36 @@ pub fn joke() {
     hell.push(' ');
     hell.push_str("steak");
     println!("{}",hell);
+    println!("{} capacity",hell.capacity());
+    println!("{} from jjj capa",String::from("jjj").capacity());
+    println!("{} from jj j capa",String::from("jj j").capacity());
+    println!("{} from ' ' capa",String::from(" ").capacity());
+    println!(" is empty '' {}",String::from("").is_empty());
+    println!(" is empty ' ' {}",String::from(" ").is_empty());
+    println!(" is ' ' contain '': {}",
+             String::from(
+                 " "
+             ).contains("")
+    );
+    println!(" is ' ' contain ' ':  {}",
+             String::from(
+                 " "
+             ).contains(" ")
+    );
+    println!(" is ' ' contain '         ':  {}",
+             String::from(
+                 " "
+             ).contains("        ")
+    );
+    println!("replace {}'s {} with {} = {}",
+             String::from(
+                 "Hall of Hog"
+             ),"Hog","Fame",
+             String::from("Hall of Hog").replace(
+                 "Hog","Fame"
+             )
+    );
+    for k in "w ta bas ic bitch".split_whitespace() {
+        println!("{}",k);
+    }
 }
