432270f56305850c9802ee0f5c378e79ee18606f
adrian1230 <adrian1145d98@gmail.com>
2021-01-14 21:12:58 +0800

    adrian

--- c_c++/arr.c
+++ c_c++/arr.c
@@ -5,13 +5,16 @@ int main()
   printf("enter the length of the integer list: ");
   int n;
   scanf("%d",&n);
-  int data[] = {};
-  while ((int) (sizeof(data)/sizeof(data[0])) != n ) {
-    int len = (int) sizeof(data) / sizeof(data[0]);
+  int data[n];
+  while ((int) (sizeof(data)/sizeof(data[0])) != n)
+  {
+    int len = (int) (sizeof(data)/sizeof(data[0])); 
     int val;
-    printf("enter a value as of integer: ");
+    printf("give me an int: ");
     scanf("%d",&val);
     data[len] = val;
   }
+  printf("%d \n",(int) (sizeof(data)/sizeof(data[0])));
+  printf("%d \n",data[-1]);
   return 0;
 }
