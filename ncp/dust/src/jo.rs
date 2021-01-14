13dd70adf76298773c5304a5afad39d7c96f8a7e
adrian1230 <adrian1145d98@gmail.com>
2021-01-15 04:36:04 +0800

    adrian

--- ncp/dust/src/jo.rs
+++ ncp/dust/src/jo.rs
@@ -51,13 +51,6 @@ pub fn haha()
 
 pub fn ty()
 {
-    let x = 1;
-    let y = 2.6;
-    let j : i64 = 11;
-    println!("x i32 1 {} y i64 2.6 {} j i64 11 {}",
-             x,y,j);
-    let u: i64 = 4545445454545;
-    println!("u: i64 {}",u);
     // Find max size
     println!("Max i32: {}", std::i32::MAX);
     println!("Max i64: {}", std::i32::MAX);
