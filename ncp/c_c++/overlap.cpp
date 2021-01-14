d5d502a4d2c9e7eaa631054cf011bab62370d723
adrian1230 <adrian1145d98@gmail.com>
2021-01-15 00:12:24 +0800

    adrian

--- ncp/c_c++/overlap.cpp
+++ ncp/c_c++/overlap.cpp
@@ -10,6 +10,25 @@ int main()
   cout << "enter the length of the Big array: " << endl;
   cin >> n;
   std::vector<int> data;
-  int arr[2];
+  int c = 0;
+  while (c != n)
+  {
+    int arr[2];
+    int val_1;
+    printf("1st val: ");
+    scanf("%d",&val_1);
+    int val_2;
+    printf("2nd val: ");
+    scanf("%d",&val_2);
+    arr[0] = val_1;
+    arr[1] = val_2;
+    data.insert(data.end(),std::begin(arr),std::end(arr));
+    c += 1;
+  }
+  int len = sizeof(data) / sizeof(data[0]);
+  for (int u = 0; u < len; u++)
+  {
+    printf("%d \n",data[u]);
+  }
   return 0;
 }
