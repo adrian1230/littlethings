76cbed138c4bcaff9544645ec7cc8a716462d443
adrian1230 <adrian1145d98@gmail.com>
2021-01-14 23:38:41 +0800

    adrian

---
+++ ncp/c_c++/overlap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "enter the length of the Big array: " << endl;
+  cin >> n;
+  std::vector<int> data;
+  int arr[2];
+  return 0;
+}
