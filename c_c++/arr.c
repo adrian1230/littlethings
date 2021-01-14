a142b6559965b94a06c07b0a3cbfb93d364ed3e5
adrian1230 <adrian1145d98@gmail.com>
2021-01-14 21:43:54 +0800

    adrian

--- c_c++/arr.c
+++ c_c++/arr.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+Given a 2D array arr[][] with each row of the form {l, r}, 
+the task is to find a pair (i, j) such that 
+the ith interval lies within the jth interval. 
+If multiple solutions exist, then print anyone of them. 
+Otherwise, print -1.
+*/
+
 int main()
 {
   printf("enter the length of the integer list: ");
@@ -8,14 +16,17 @@ int main()
   int data[n];
   for (int i = 0; i < n; ++i)
   {
+    printf("n's val must smaller than (n + 1)'s val where n is an order number");
     int arr[2];
-    for (int t = 0; t < 2; ++t)
-    {
-    }
-    int val;
-    printf("give me an int: ");
-    scanf("%d",&val);
-    data[i] = val;
+    printf("the first value: ");
+    int val_1;
+    scanf("%d",&val_1);
+    printf("the second value: ");
+    int val_2;
+    scanf("%d",&val_2);
+    arr[0] = val_1;
+    arr[1] = val_2;
+    // data[i] = arr;
   }
   return 0;
 }
