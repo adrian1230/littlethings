b7bce9e0daec8d7ccdee1e7dcc4cac7901f4d809
adrian1230 <adrian1145d98@gmail.com>
2021-01-16 04:07:22 +0800

    adrian

---
+++ ncp/egg2/floor.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+unknown = np.random.randint(1,101) # 18
+# break x => 19
+# save x <= 18
+# 0 <= x <= 100 
+
+c = 0
+
+egg = 2
+
+same = 0
+
+s_f = np.random.randint(1,101)
+
+while egg != 0:
+    print(s_f)
+    if s_f < unknown: # 6 < 18
+        s_f = np.random.randint(s_f,101)
+        c+=1
+        pass
+    elif s_f == unknown: # 18 == 18
+        s_f = np.random.randint(s_f,101)
+        c+=1
+        same += 1
+        pass
+    else: # 20 > 18
+        s_f = np.random.randint(1,s_f+1)
+        c+=1
+        egg -= 1
+    if same == 1:
+        if s_f == (unknown + 1):
+            print("get it")
+
+print(c)
+print(unknown)
+
+# limit = 18;
+# f = 13;     f = 20;
+# egg = 2;    egg = 1; 
+# f = 18;     f = 16;
+# egg = 2;    egg = 1;
+# f = 25;     f = 18;
+# egg = 1;    egg = 1;
+# f = 23;     f = 19;
+# egg = 0;    egg = 0;
