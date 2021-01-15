ef5890f7a658fbdb15efb7ba6d7b52d905cafcee
adrian1230 <adrian1145d98@gmail.com>
2021-01-15 14:22:44 +0800

    adrian

--- ncp/dust/src/main.rs
+++ ncp/dust/src/main.rs
@@ -6,4 +6,11 @@ fn main() {
    jo::haha();
    arr::list();
    jo::ty();
+
+   println!("10 => binary {:\
+    b} hex {:\
+    x} octal {:o\
+    }",10,10,10
+   );
+   println!("{:?}",(12,true,"he"));
 }
