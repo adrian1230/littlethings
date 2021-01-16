bfcd52d72a57ee8d209e48746e4022071bc87e2c
adrian1230 <adrian1145d98@gmail.com>
2021-01-17 03:54:59 +0800

    adrian

--- ncp/egg2/floor.py
+++ ncp/egg2/floor.py
@@ -11,24 +11,34 @@ egg = 2
 
 same = 0
 
+trick = 0
+
 s_f = np.random.randint(1,101)
 
+# 9:2 18:2 20:1
+# 9:2 20:1 18:1
+
 while egg != 0:
-    print("current selected floor: {}".format(s_f))
-    if s_f < unknown: # 6 < 18
-        s_f = np.random.randint(s_f+1,101)
-        pass
-    elif s_f == unknown: # 18 == 18
-        s_f = np.random.randint(s_f+1,101)
-        same += 1
-        pass
-    else: # 20 > 18
-        s_f = np.random.randint(1,s_f)
-        egg -= 1
+    if s_f == unknown + 2:
+        trick += 1
     if same == 1:
         if s_f == (unknown + 1):
             print("get it")
             break
+        elif trick == 1:
+            print("get it")
+            break
+        else:
+            pass
+    print("current selected floor: {}".format(s_f))
+    if s_f < unknown: # 6 < 18
+        s_f = np.random.randint(s_f+1,101)
+    elif s_f == unknown: # 18 == 18
+        s_f = np.random.randint(s_f+1,101)
+        same += 1
+    else: # 25 > 18
+        s_f = np.random.randint(1,s_f)
+        egg -= 1
     c += 1
     print("c: {}".format(c))
     print("egg: {}".format(egg))
@@ -37,11 +47,11 @@ while egg != 0:
 print("the limit: {}".format(unknown))
 
 # limit = 18;
-# f = 13;     f = 20;
-# egg = 2;    egg = 1; 
-# f = 18;     f = 16;
-# egg = 2;    egg = 1;
-# f = 25;     f = 18;
+# f = 13;     f = 20;      f = 20;
+# egg = 2;    egg = 1;     egg = 1;
+# f = 18;     f = 16;      f = 18;
+# egg = 2;    egg = 1;     egg = 1;
+# f = 25;     f = 18;      break; as 18 worked not 20; so 19 must be limit + 1
 # egg = 1;    egg = 1;
 # f = 23;     f = 19;
 # egg = 0;    egg = 0;
