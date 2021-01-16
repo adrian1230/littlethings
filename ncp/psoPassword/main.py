e2c9e3a6ace2c2abb29a67681bba4145d312c97e
adrian1230 <adrian1145d98@gmail.com>
2021-01-17 05:31:22 +0800

    adrian

---
+++ ncp/psoPassword/main.py
@@ -0,0 +1,10 @@
+# input 1 - 1000000 long string.
+# find password met the qualify of prefix, suffix, and obelix.
+# else: "Just a legend" without quote in return.
+
+# time limit: 2 secs
+# memory limit: 256 mb
+# input & output: standard
+
+a = input("string without break and from len 1 to len 1000000:\n")
+
