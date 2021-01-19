79eb2ac92f492e24ac3861b82590c368c021b6a2
adrian1230 <adrian1145d98@gmail.com>
2021-01-20 02:57:36 +0800

    adrian

---
+++ ncp_algor/lite/century.pass.py
@@ -0,0 +1,11 @@
+# input => 1
+# check to be strong
+# 6 <= x <= 20
+# lower() in x => 1
+# upper() in x => 1
+# isdigit() in x => 1
+# non-3-recurring letters
+# 2-recurring if former requirements are met
+
+import numpy as np
+
