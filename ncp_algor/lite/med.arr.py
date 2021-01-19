2bed25ef1604c93b418c8920b3d6b639b7e2a9e5
adrian1230 <adrian1145d98@gmail.com>
2021-01-20 02:26:19 +0800

    adrian

--- ncp_algor/lite/med.arr.py
+++ ncp_algor/lite/med.arr.py
@@ -14,24 +14,23 @@ a2 = a1
 while a1 == a2:
     a2 = np.random.randint(0,1001)
 
-while len(arr1) != 10:
+while len(arr1) != a1:
     val = np.random.randint(-1000000,1000001)
     arr1.append(val)
 
-while len(arr2) != 10:
+while len(arr2) != a2:
     val = np.random.randint(-1000000,1000001)
     arr2.append(val)
 
 arr = arr1 + arr2
 arr = sorted(arr)
 
-print(arr)
-
 if (a1+a2) % 2 == 0:
     m = (a1+a2) / 2
     m = int(m)
-    print(m)
+    print((arr[m-1]+arr[m])/2)
+
 else:
     m = (a1+a2) / 2 - 0.5
     m = int(m)
-    print(m)
+    print(arr[m])
