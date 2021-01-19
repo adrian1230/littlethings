e5f9c540df991145fc85c9efdd512457fd1fb84e
adrian1230 <adrian1145d98@gmail.com>
2021-01-20 03:00:41 +0800

    adrian

--- ncp_algor/lite/century.pass.py
+++ ncp_algor/lite/century.pass.py
@@ -1,4 +1,5 @@
-# input => 1
+# 1 <= input <= 50
+# '.' & '!' are allowed
 # check to be strong
 # 6 <= x <= 20
 # lower() in x => 1
@@ -6,6 +7,12 @@
 # isdigit() in x => 1
 # non-3-recurring letters
 # 2-recurring if former requirements are met
+# return 0 if password is strong
+# return mini-step for turning the password into a strong one
+# in 1 step
+# you can insert a character
+# you can delete a character
+# you can replace a character with another
 
 import numpy as np
 
