19a213e354453244c7e7d2d6a726adc644100c66
adrian1230 <adrian1145d98@gmail.com>
2021-01-21 21:13:56 +0800

    adrian

--- ncp_algor/lite/century.pass.py
+++ ncp_algor/lite/century.pass.py
@@ -170,8 +170,11 @@ if check == 1:
         else:
             raise ValueError("not allowed to be in pass")
     comb = [theta_,beta_,alpha_1,alpha_2]
-    # if theta_ == 0:
-        
+    ination = [theta,beta,alpha1,alpha2]
+    if theta_ == 0:
+        max_ = max(comb)
+        position = [i for i in range(len(pass_)) if pass_[i] in ination[max_]]
+        pass_ = pass_.replace(pass_[position[np.random.randint(len(position))]],ination[max_][np.random.randint(len(ination[max_]))],1)
     print(theta_,beta_,alpha_1,alpha_2)    
     print(pass_,counter)
     
