f80e76ff73b39fd6f782b9381e76a25cb397075d
adrian1230 <adrian1145d98@gmail.com>
2021-01-25 21:53:47 +0800

    adrian

--- ncp_algor/cactus/src/array.rs
+++ ncp_algor/cactus/src/array.rs
@@ -7,5 +7,22 @@ pub fn generate()
     let n2: u16 = random.gen();
     println!("{}",n1);
     println!("{}",n2);
-    println!("{}",random.gen::<u32>())
+    println!("{}",random.gen::<u32>());
+    let mut v: Vec<i32> = Vec::new();
+    while v.len() != 5 {
+        let val = rand::thread_rng(
+        ).gen_range(0..10);
+        v.push(val);
+    }
+    println!("");
+    for k in &v {
+        println!("{}",k);
+    }
+    println!("{:?}",v);
+    // println!("{:?}",v.iter().min())
+    let min_v = v.iter().min();
+    match min_v {
+        Some(min) => println!("{:?}",min),
+        None => println!("Nothing")
+    }
 }
