60d68478bb32a283f5b7304531870bcddab30712
adrian1230 <adrian1145d98@gmail.com>
2021-01-25 22:16:25 +0800

    adrian

--- ncp_algor/cactus/src/array.rs
+++ ncp_algor/cactus/src/array.rs
@@ -2,27 +2,32 @@ use rand::Rng;
 
 pub fn generate()
 {
-    let mut random = rand::thread_rng();
-    let n1: u8 = random.gen();
-    let n2: u16 = random.gen();
-    println!("{}",n1);
-    println!("{}",n2);
-    println!("{}",random.gen::<u32>());
+    // let mut random = rand::thread_rng();
+    // let n1: u8 = random.gen();
+    // let n2: u16 = random.gen();
+    // println!("{}",n1);
+    // println!("{}",n2);
+    // println!("{}",random.gen::<u32>());
     let mut v: Vec<i32> = Vec::new();
     while v.len() != 5 {
         let val = rand::thread_rng(
         ).gen_range(0..10);
         v.push(val);
     }
-    println!("");
-    for k in &v {
-        println!("{}",k);
-    }
+    // println!("");
+    // for k in &v {
+    //     println!("{}",k);
+    // }
     println!("{:?}",v);
     // println!("{:?}",v.iter().min())
-    let min_v = v.iter().min();
-    match min_v {
-        Some(min) => println!("{:?}",min),
-        None => println!("Nothing")
-    }
+    // let min_v = v.iter().min();
+    // let max_v = v.iter().max();
+    // match min_v {
+    //     Some(min) => println!("{:?}",min),
+    //     None => println!("Nothing")
+    // }
+    // match max_v {
+    //     Some(max) => println!("{:?}",max),
+    //     None => println!("Nothing")
+    // }
 }
